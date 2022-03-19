6fe093c837470398bb1a6f1554bf7a4e5ba08ec3
Matthew Little <zone117x@gmail.com>
2022-03-19 13:57:33 +0100

    fixes

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
-# Instructions
+# Usage Instructions
 
 ### Requirements:
+ * Docker Compose
+ * At least 100GB of free disk space
  * Ubuntu-20.x (if using the bootstrap script)
- * Docker
 
+#### Clone repo
+```shell
+git clone https://github.com/zone117x/tbtc-mining.git
+cd tbtc-mining/
+```
+
+#### Configure mining reward address
+Specify the testnet btc address in the `TBTC_ADDRESS` environment variable inside `docker-compose.yml`
 
 #### Optionally, to speed up testnet chain sync, use the bootstrap bitcoin testnet chain data with:
 ```shell
--- bootstrap-chaindata.sh
+++ bootstrap-chaindata.sh
@@ -10,5 +10,20 @@ if find "$output_dir" -mindepth 1 -maxdepth 1 | read; then
     exit 1
 fi
 
-curl -O https://m-shared-public.s3.eu-central-1.amazonaws.com/bitcoin-testnet-chain.tar.zst
-tar xaf bitcoin-testnet-chain.tar.zst -C "$output_dir"
+output_file=bitcoin-testnet-chain.tar.zst
+
+if [ -e "$output_file" ]
+then
+    echo "Chainstate archive file already exists $output_file ... skipping download"
+else
+    curl --progress-bar -o $output_file https://m-shared-public.s3.eu-central-1.amazonaws.com/bitcoin-testnet-chain.tar.zst
+fi
+
+if ! command -v pv &> /dev/null
+then
+    tar xaf $output_file -C "$output_dir" --verbose
+else
+    pv $output_file | tar --use-compress-program=unzstd -x -C "$output_dir"
+fi
+
+
