60f08139dce5ed9b6e7e32f5a65fce594878074e
Matthew Little <zone117x@gmail.com>
2023-03-07 22:41:58 +0100

    fix: update bootstrap script

--- bootstrap-chaindata.sh
+++ bootstrap-chaindata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 dir="$(dirname "$(realpath "$0")")"
-output_dir="$dir/bitcoin-testnet-data/testnet3/"
+output_dir="$dir/data/bitcoin/testnet3/"
 echo "Output dir: $output_dir"
 mkdir -p $output_dir
 
